abef69cfd40cfceed4a23dd975a0a67648cdee22
mgc480 <masonclayton@live.com>
2012-10-01 03:32:44 -0700

    adding simple file loader WIP

--- ccvrc/GuiLoader.cpp
+++ ccvrc/GuiLoader.cpp
@@ -9,5 +9,16 @@ GuiLoader::~GuiLoader(void){
 }
 
 void GuiLoader::LoadFile(std::string path){
+	std::string line = "";
+	std::ifstream file(path);
 
+	if(file.is_open()){
+		while(file.good()){
+			std::getline(file, line);
+
+
+		}
+	}else{
+		std::cout << "unable to open file: \"" << path << "\"" << std::endl;
+	}
 }
