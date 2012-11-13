d5222a3ead3344774c5c278d2ec326bfa6f34772
mgc480 <masonclayton@live.com>
2012-11-12 21:57:58 -0800

    adding iterface functions for lua (c++ functions are done)

--- ccvrc/luaInterface.cpp
+++ ccvrc/luaInterface.cpp
@@ -13,14 +13,33 @@ luaInterface::~luaInterface(void){
 	lua_close(luaState);
 }
 
-bool luaInterface::luaInit(){
-	return true;
+bool luaInterface::luaInit(void){
+	bool status;
+
+	lua_getglobal(luaState, "init");
+
+	lua_call(luaState, 0, 1);
+	status = lua_toboolean(luaState, -1);
+	lua_pop(luaState, 1);
+
+	return status;
 }
 
 void luaInterface::luaEvent(int target, const char * type){
+	lua_getglobal(luaState, "event");
 
+	lua_pushnumber(luaState, target);
+	lua_pushstring(luaState, type);
+
+	lua_call(luaState, 2, 0);
+	lua_pop(luaState, 1);
 }
 
 void luaInterface::luaError(const char * type){
+	lua_getglobal(luaState, "error");
 
+	lua_pushstring(luaState, type);
+
+	lua_call(luaState, 1, 0);
+	lua_pop(luaState, 1);
 }
--- ccvrc/luaInterface.h
+++ ccvrc/luaInterface.h
@@ -14,9 +14,12 @@ class luaInterface{
 public:
 	luaInterface(const char * path);
 	~luaInterface(void);
-	bool luaInit();
+	bool luaInit(void);
 	void luaEvent(int target, const char * type);
 	void luaError(const char * type);
+
+protected:
+	virtual void addSprite();
 
 private:
 	lua_State* luaState;
