e7dd50045d271993b1d4be1a5b6b59338e8d250a
mgc480 <masonclayton@live.com>
2012-11-25 15:21:35 -0800

    new lines work with string text

--- ccvrc/lua/main_menu.lua
+++ ccvrc/lua/main_menu.lua
@@ -1,6 +1,7 @@
 --addPolySprite(1, 0, 0, 100, 100, 0xc0c0c0);
 --addPolySprite(2, -3, -3, 100, 100, 0xcc0000);
-addSprite(2, 20, 20, "tmp_cen.png", 0, 0);
+--addSprite(2, 20, 20, "tmp_cen.png", 0, 0);
+addStringSprite(5, 100, 100, 75, "testing \n new line", 0x0000ff);
 --addSprite(1, 601, 20, "tmp_cen.png", -601, 600); addPolySprite
 
 --addStringSprite(1, 2, 3, 4, 5);
