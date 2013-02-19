9ca4766b380ea9afb95312c0028a66cf494b383a
mgc480 <masonclayton@live.com>
2013-02-18 21:39:35 -0800

    added individual bar icons for health, mana...

--- ccvrc/lua/battle.lua
+++ ccvrc/lua/battle.lua
@@ -6,7 +6,7 @@ addSprite("bottom_bar_bg", 0, 440, "bottom_bar_bg.png", 0, 0);
 --top bar
 addSprite("top_bar_bg", 0, -20, "top_bar_bg.png", 0, 0);
 addStringSprite("enemy_bar_text", 10, 0, 32, "Enemy: ", 0x000000);
-addSprite("top_bar_health_bg", 0, 0, "bar.png", 800, 16);
+addSprite("top_bar_health_bg", 0, 0, "health_bar.png", 800, 16);
 
 function init()
 	stage = 0;	--the current step that the ui is index
