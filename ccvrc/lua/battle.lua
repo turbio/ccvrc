72f9972f311dcc99e9c662bc01de7ccca60a732f
mgc480 <masonclayton@live.com>
2013-02-24 21:21:09 -0800

    added enemy health bar

--- ccvrc/lua/battle.lua
+++ ccvrc/lua/battle.lua
@@ -1,3 +1,5 @@
+enemy_health = 100;
+
 addSprite("bg", 0, 0, "main_menu_bg.png", 0, 0);
 
 --bottom bar
@@ -5,8 +7,10 @@ addSprite("bottom_bar_bg", 0, 440, "bottom_bar_bg.png", 0, 0);
 
 --top bar
 addSprite("top_bar_bg", 0, -20, "top_bar_bg.png", 0, 0);
-addStringSprite("enemy_bar_text", 10, 0, 32, "Enemy: ", 0x000000);
-addSprite("top_bar_health_bg", 0, 0, "health_bar.png", 800, 16);
+addStringSprite("enemy_bar_text", 10, -3, 32, "Enemy: ", 0x000000);
+
+addSprite("enemy_health_bar_bg", 110, 7, "bar_bg.png", 675, 25);
+addSprite("enemy_health_bar", 110, 7, "health_bar.png", 675, 25);
 
 function init()
 	stage = 0;	--the current step that the ui is index
